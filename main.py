ae2f65b27c3d6e64f730b7cc2726de68f44fb6a7
Suraj <98628922+Suraj0-10@users.noreply.github.com>
2022-05-29 15:30:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import telebot
+import json
+import math
+
+import requests
+
+from DaisyX.decorator import register
+
+from .utils.disable import disableable_dec
+from .utils.message import get_args_str
+
 API_KEY = "5299193377:AAHEGIBIqtPeqVcnJ6GiQcteofe0W4GrmF8"
 
 bot = telebot.TeleBot(API_KEY,parse_mode=None)
@@ -13,4 +23,16 @@ def send_start(hlp):
 @bot.message_handler(commands=["info"])
 def send_start(inf):
     bot.reply_to(inf,"Basically @zoro_to made this bot to test new codes and run it ")
+
+
+@register(cmds=["math", "simplify"])
+@disableable_dec("math")
+async def _(message):
+    args = get_args_str(message)
+    response = requests.get(f"https://newton.now.sh/api/v2/simplify/{args}")
+    c = response.text
+    obj = json.loads(c)
+    j = obj["result"]
+    await message.reply(j)
+
 bot.polling()
