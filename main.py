c309ffa0de11a1b291807913d2b2b53cebbd6915
Suraj <98628922+Suraj0-10@users.noreply.github.com>
2022-05-29 14:07:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ API_KEY = "5299193377:AAHEGIBIqtPeqVcnJ6GiQcteofe0W4GrmF8"
 bot = telebot.TeleBot(API_KEY,parse_mode=None)
 @bot.message_handler(commands=["start"])
 def send_start(strt):
-    bot.reply_to(strt,"bsdk")
+    bot.reply_to(strt,"Hey! I'm a test or Practice bot of @Zoro_to")
 bot.polling()
