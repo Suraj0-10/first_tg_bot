3f356a0edc06926da0580beec43551d56f196018
Suraj <98628922+Suraj0-10@users.noreply.github.com>
2022-05-30 19:54:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
-import telebot
-
-API_KEY = "5261660867:AAE6JWmiwg65idb0KR0tuyyHwpeNGpDprBQ"
-
-bot = telebot.TeleBot(API_KEY,parse_mode=None)
-@bot.message_handler(commands=["start"])
-def send_start(strt):
-    bot.reply_to(strt,"Hey! I'm a test or Practice bot of @Zoro_to\nYou can try out my commands\n/help\n/info\n/start")
-
-@bot.message_handler(commands=["help"])
-def send_start(hlp):
-    bot.reply_to(hlp,"There's nothing to help ask @zoro_to he may help u")
-
-@bot.message_handler(commands=["info"])
-def send_start(inf):
-    bot.reply_to(inf,"Basically @zoro_to made this bot to test new codes and run it ")
-
-@bot.message_handler(commands=["text"])
-def send_start(inf):
-    bot.reply_to(inf,"none")
-
-@bot.message_handler(commands=["aj"])
-def send_start(inf):
-    bot.reply_to(inf,"thanks @anime_in_30mb for this amazing channel\nI hope u do well in exams @Assassin_Aj")
-
-bot.polling()
+from pyrogram import Client, filters
+from pyrogram.types import ChatPermissions
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+bot = Client("bot",
+            api_id = 5168062,
+            api_hash = "04c049aa96d1cc87920b45b7fb43c0d0",
+            bot_token = "5261660867:AAE6JWmiwg65idb0KR0tuyyHwpeNGpDprBQ"
+            )
+@bot.on_message(filters.command("start"))
+def cmd1(bot,message):
+ bot.send_message(message.chat.id,"hi")
+#button
+START_MESSAGE = "MY CHANNELS"
+START_MESSAGE_BUTTON = [
+  [InlineKeyboardButton
+   ('MOVIES',url='t.me/PX_07')
+  ],
+  [InlineKeyboardButton
+   ('SERIES',url='t.me/seriez_encoded')
+  ]
+]
+@bot.on_message(filters.command("channel"))
+def channel(bot,message):
+ text = START_MESSAGE
+ reply_markup =  InlineKeyboardMarkup(START_MESSAGE_BUTTON)
+ message.reply(
+  text = text,
+  reply_markup = reply_markup,
+  )
+@bot.on_message(filters.text)
+def cmd1(client,message):
+ message.reply_text(message.text)
+print("started")
+bot.run()
